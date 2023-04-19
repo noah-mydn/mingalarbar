b29bfda10a71c40a2cc0c2ffe32edb8dfe567e88
May <mayyadanar@hotmail.com>
2023-04-19 13:53:15 +0630

    test

---
+++ src/components/MenuDetail.jsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+export const MenuDetail = () => {
+  return (
+    <Container my={2} bgcolor='red'>
+        
+    </Container>
+  )
+}
